1cf4ff970cc2956a0239acfdbed198db54c297ff
AdrianLazzi <85973189+AdrianLazzi@users.noreply.github.com>
2024-08-27 14:17:26 -0700

    Update streamlit_final.py

--- streamlit_final.py
+++ streamlit_final.py
@@ -7,9 +7,22 @@ from matplotlib.ticker import MaxNLocator
 import numpy as np
 import streamlit as st
 
+firebase_config = {
+    "type": st.secrets["firebase"]["type"],
+    "project_id": st.secrets["firebase"]["project_id"],
+    "private_key_id": st.secrets["firebase"]["private_key_id"],
+    "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+    "client_email": st.secrets["firebase"]["client_email"],
+    "client_id": st.secrets["firebase"]["client_id"],
+    "auth_uri": st.secrets["firebase"]["auth_uri"],
+    "token_uri": st.secrets["firebase"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+}
+
 # Firebase configuration
 if not firebase_admin._apps:
-    cred = credentials.Certificate('cred.json')
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://huntingtonhospital-default-rtdb.firebaseio.com/'
     })
